7bf811fb2a33674d70aa087321399c1da7d4c879
OmarAfet <OmarAlenezy2004@gmail.com>
2023-07-11 13:54:28 +0300

    Fix duplicated elements

--- Python/Easy/العناصر المتشابهة في المصفوفة.py
+++ Python/Easy/العناصر المتشابهة في المصفوفة.py
@@ -7,7 +7,7 @@ def get_duplicate_elements(arr: list[int]) -> list[int]:
     x = []
     y = []
     for i in arr:
-        if i in x:
+        if i in x and i not in y:
             y.append(i)
             pass
         else:
